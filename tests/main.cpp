25342fec0b45950ddd282fef78ec395be7e59278
martejulie <m.j.satra@fys.uio.no>
2015-11-25 18:37:35 +0100

    litt debug

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,7 +7,7 @@ TEST_CASE( "MyExperiment", "[myexperiment]" ) {
         string testfile = "121115_dataSetForTestingPurpose_r_cap_1_rt_150_p_cap_70_M_0_0011_149.dat";
         Experiment my = Experiment(testfile);
         REQUIRE(my.countElements("1 2 3") == 3);
-        REQUIRE(my.get_n("1") == 149);
+        REQUIRE(my.get_n("1") == 1);
         REQUIRE(my.get_n("1 2 3") == 0);
     }
 }
