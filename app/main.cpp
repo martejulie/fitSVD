88ef93ac858f97d6220b2ff5685dbfaef68e1c24
martejulie <m.j.satra@fys.uio.no>
2015-11-25 19:09:55 +0100

    prøve igjen

--- app/main.cpp
+++ app/main.cpp
@@ -8,7 +8,7 @@ int main()
     string testfile = "121115_dataSetForTestingPurpose_r_cap_1_rt_150_p_cap_70_M_0_0011_149.dat";
     Experiment my = Experiment(testfile);
     cout << my.countElements("1 2 3") << endl;
-    my.check_sigma("1 2 3 4");
+    //my.check_sigma("1 2 3 4");
 
     return 0;
 }
