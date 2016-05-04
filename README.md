2da3831c88f9aaa23744f0909d8af49d28a55096
martejulie <m.j.satra@fys.uio.no>
2016-05-04 15:10:34 +0200

    readme

--- README.md
+++ README.md
@@ -1,5 +1,44 @@
 # fitSVD
 Modeling of Data by Use of Singular Value Decomposition (SVD)
+Details of the code are given in Chapter 4 of
+the Master's degree thesis "Estimation of oxygen consumption from partial pressure
+gradients in cortex", by Marte Julie Sætra, May 2016.
 
+## Code Example
+You are given a set of experimental data points and want to study it by fitting it to a model of choice.
+The model must be given on a form that is linear with respect to its unknowm parameters.
+
+The model is defined by a set of basis functions and given in the `model` class. The basis functions could for
+instance look like the following
+``` cpp
+Col<double> Model::funcs(double x) {
+// basis functions
+Col<double> f(3) ;
+f[0] = 1.0;
+f[1] = x ;
+f[ 2 ] = x∗x ;
+return f ;
+}
+```
+
+The `Experiment` class takes the name of a datafile as argument, which may look at the following
+```
+414
+2.502999 81.091224 1.262092
+7.898756 72.514262 2.010258
+   ...       ...      ...
+```
+First line contains the number of data points. The coloumns contains the value of x, y and measurment errors `sigma`,
+respectively. You construct an `Experiment` object in `main.cpp`
+
+When you have defined your model, and have a datafile with values of your data points, all you have to do
+to solve the optimization problem is to construct a `Fitsvd` object in `main.cpp`. The constructor of `Fitsvd`
+calls the relevant function, which is named `fit`. The construcor takes the model `funcs`, the experimental data
+`x`, `y` and `sigma` and a theshold value `thresh` as arguments:
+``` cpp
+Fitsvd myFit = Fitsvd(&Model::funcs ,myExperiment.x, myExperiment.y ,myExperiment.sigma, thresh);
+```
+
+## Testing
 [![Build Status](https://travis-ci.org/martejulie/fitSVD.svg)](https://travis-ci.org/martejulie/fitSVD)
 
--- app/main.cpp
+++ app/main.cpp
@@ -14,12 +14,100 @@ double KroghModel_std3param::m;
 
 // !set variables!
 double KroghModel_std2param::r_cap = 6;
-double KroghModel_std2param::r_t = 100;
+double KroghModel_std2param::r_t = 140;
 
 double KroghModel_std3param::r_cap = 6;
 
 int main()
 {
+    // 2 param
+    ofstream outFile;
+    outFile.open("040416_svd2paramFit_rcap-10_rt-140.dat", ios::out);
+
+    int N = 15;
+    vector<string> filename;
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset1.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset2.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset3.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset4.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset5.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset6.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset7.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset8.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset9.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset10.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset11.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset12.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset13.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset14.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset15.dat");
+
+    for (int i = 0; i < N; i++) {
+        Experiment myExp = Experiment(filename[i]);
+        Fitsvd myFit = Fitsvd(&KroghModel_std2param::funcs, myExp.x, myExp.y, myExp.sigma, 1e-12);
+        myFit.fit();
+        KroghModel_std2param::set_parameters(myFit.a);
+        outFile << filename[i] << endl;
+        outFile << "a0: " << myFit.a[0] << endl;
+        outFile << "a1: " << myFit.a[1]<< endl;
+        outFile << "p_cap: " << KroghModel_std2param::p_cap << endl;
+        outFile << "M: " << KroghModel_std2param::m << endl;
+        outFile << "reduced_chisq: " << myFit.reduced_chisq << endl;
+        outFile << "C00: " << myFit.covar(0,0) << endl;
+        outFile << "C10: " << myFit.covar(1,0) << endl;
+        outFile << "C11: " << myFit.covar(1,1) << endl;
+        outFile << endl;
+    }
+    outFile.close();
+
+    /*
+    // 3 param
+    ofstream outFile;
+    outFile.open("040416_svd3paramFit_rcap-6.dat", ios::out);
+
+    int N = 15;
+    vector<string> filename;
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset1.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset2.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset3.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset4.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset5.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset6.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset7.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset8.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset9.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset10.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset11.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset12.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset13.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset14.dat");
+    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset15.dat");
+
+    outFile << "r_cap: " << KroghModel_std3param::r_cap << endl;
+    for (int i = 0; i < N; i++) {
+        Experiment myExp = Experiment(filename[i]);
+        Fitsvd myFit = Fitsvd(&KroghModel_std3param::funcs, myExp.x, myExp.y, myExp.sigma, 1e-12);
+        myFit.fit();
+        KroghModel_std3param::set_parameters(myFit.a);
+        outFile << filename[i] << endl;
+        outFile << "a0: " << myFit.a[0] << endl;
+        outFile << "a1: " << myFit.a[1]<< endl;
+        outFile << "a2: " << myFit.a[2]<< endl;       
+        outFile << "r_t: " << KroghModel_std3param::r_t << endl;
+        outFile << "p_cap: " << KroghModel_std3param::p_cap << endl;
+        outFile << "M: " << KroghModel_std3param::m << endl;
+        outFile << "reduced_chisq: " << myFit.reduced_chisq << endl;
+        outFile << "C00: " << myFit.covar(0,0) << endl;
+        outFile << "C10: " << myFit.covar(1,0) << endl;
+        outFile << "C20: " << myFit.covar(2,0) << endl;
+        outFile << "C11: " << myFit.covar(1,1) << endl;
+        outFile << "C21: " << myFit.covar(2,1) << endl;
+        outFile << "C22: " << myFit.covar(2,2) << endl;
+        outFile << endl;
+    }
+    outFile.close();
+    */
+
 
     // 2 param, 15 data sets, study the threshold tolerance
     /*
@@ -136,93 +224,6 @@ int main()
     outFile.close();
     */
 
-    // 2 param, 15 data sets
-    /*
-    ofstream outFile;
-    outFile.open("091215_svd2param_fit_r_cap_3_r_t_150.dat", ios::out);
-
-    int N = 15;
-    vector<string> filename;
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_August_30_2010_20100830_Place4_grid_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_04_2010_20100904_Place1_grid_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_07_2010_20100907_Place4_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_07_2010_20100907_Place5_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_07_2010_20100907_Place6_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_07_2010_20100907_Place11_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_07_2010_20100907_Place12_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_07_2010_20100907_Place14_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_07_2010_20100907_Place15_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_07_2010_20100907_Place16_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_08_2010_20100908_Place5_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_08_2010_20100908_Place6_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_08_2010_20100908_Place7_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_08_2010_20100908_Place14_Combined.dat");
-    filename.push_back("/home/martejulie/master_project/experimental_data/pO2_student_data/dat_files/n_r_po2_data_from_space_md9_4_adev_PO2_September_08_2010_20100908_Place15_Combined.dat");
-
-    for (int i = 0; i < N; i++) {
-        Experiment myExp = Experiment(filename[i]);
-        Fitsvd myFit = Fitsvd(&KroghModel_std2param::funcs, myExp.x, myExp.y, myExp.sigma);
-        myFit.fit();
-        KroghModel_std2param::set_parameters(myFit.a);
-        outFile << filename[i] << endl;
-        outFile << "a0: " << myFit.a[0] << endl;
-        outFile << "a1: " << myFit.a[1]<< endl;
-        outFile << "p_cap: " << KroghModel_std2param::p_cap << endl;
-        outFile << "M: " << KroghModel_std2param::m << endl;
-        outFile << "reduced_chisq: " << myFit.reduced_chisq << endl;
-        outFile << "C00: " << myFit.covar(0,0) << endl;
-        outFile << "C10: " << myFit.covar(1,0) << endl;
-        outFile << "C11: " << myFit.covar(1,1) << endl;
-        outFile << endl;
-    }
-    outFile.close();
-    */
-
-    // 3 param
-    ofstream outFile;
-    outFile.open("040415_svd3paramFit_rcap-6.dat", ios::out);
-
-    int N = 15;
-    vector<string> filename;
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset1.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset2.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset3.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset4.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset5.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset6.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset7.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset8.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset9.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset10.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset11.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset12.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset13.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset14.dat");
-    filename.push_back("/home/martejulie/master_project_data/dat_files/n_r_po2_data_from_dataset15.dat");
-
-    outFile << "r_cap: " << KroghModel_std3param::r_cap << endl;
-    for (int i = 0; i < N; i++) {
-        Experiment myExp = Experiment(filename[i]);
-        Fitsvd myFit = Fitsvd(&KroghModel_std3param::funcs, myExp.x, myExp.y, myExp.sigma, 1e-12);
-        myFit.fit();
-        KroghModel_std3param::set_parameters(myFit.a);
-        outFile << filename[i] << endl;
-        outFile << "a0: " << myFit.a[0] << endl;
-        outFile << "a1: " << myFit.a[1]<< endl;
-        outFile << "a2: " << myFit.a[2]<< endl;       
-        outFile << "r_t: " << KroghModel_std3param::r_t << endl;
-        outFile << "p_cap: " << KroghModel_std3param::p_cap << endl;
-        outFile << "M: " << KroghModel_std3param::m << endl;
-        outFile << "reduced_chisq: " << myFit.reduced_chisq << endl;
-        outFile << "C00: " << myFit.covar(0,0) << endl;
-        outFile << "C10: " << myFit.covar(1,0) << endl;
-        outFile << "C20: " << myFit.covar(2,0) << endl;
-        outFile << "C11: " << myFit.covar(1,1) << endl;
-        outFile << "C21: " << myFit.covar(2,1) << endl;
-        outFile << "C22: " << myFit.covar(2,2) << endl;
-        outFile << endl;
-    }
-    outFile.close();    
     cout << "Hello World!" << endl;
     return 0;
 }
